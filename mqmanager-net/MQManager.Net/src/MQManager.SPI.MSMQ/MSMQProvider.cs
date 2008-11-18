6789cd9339719900d7cc01dc6b780d32e76d5f61
j-pimp <j-pimp@c9d38202-4ff3-450d-aacb-4f6d708042ad>
2008-11-18 03:53:04 +0000

    Updated Copyright and versioning.
    git-svn-id: https://mqmanager.svn.sourceforge.net/svnroot/mqmanager/trunk@16 c9d38202-4ff3-450d-aacb-4f6d708042ad

--- mqmanager-net/MQManager.Net/src/MQManager.SPI.MSMQ/MSMQProvider.cs
+++ mqmanager-net/MQManager.Net/src/MQManager.SPI.MSMQ/MSMQProvider.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 David H. DeWolf
+ * Copyright 2005-2008 David H. DeWolf and Justin Dearing
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

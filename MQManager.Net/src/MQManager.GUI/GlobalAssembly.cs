173493ae12e469a246a06e93ce101426691e236a
j-pimp <j-pimp@c9d38202-4ff3-450d-aacb-4f6d708042ad>
2009-01-02 13:59:12 +0000

    Lars consolidated VisualizerForm and VisualizerControl into one assembly. Updated the solution and svn externals to reflect this.
    git-svn-id: https://mqmanager.svn.sourceforge.net/svnroot/mqmanager/trunk@52 c9d38202-4ff3-450d-aacb-4f6d708042ad

--- MQManager.Net/src/MQManager.GUI/GlobalAssembly.cs
+++ MQManager.Net/src/MQManager.GUI/GlobalAssembly.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.5.*")]
-[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0.1")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
